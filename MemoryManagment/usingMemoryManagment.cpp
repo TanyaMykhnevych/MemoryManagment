73ba4537ba3a25c171cad7e597bbf2b3cb0165f0
TanyaMy <tetiana.mykhnevych@nure.ua>
2017-04-24 17:57:06 +0300

    Added allocation and freeing memory.

--- MemoryManagment/usingMemoryManagment.cpp
+++ MemoryManagment/usingMemoryManagment.cpp
@@ -35,9 +35,9 @@ VOID PrintSystemInfo()
 	_tprintf(_T("dwPageSize = %d\n"), systemInfo.dwPageSize);
 	_tprintf(_T("dwAllocationGranularity = %d\n"), systemInfo.dwAllocationGranularity);
 	_tprintf(_T("lpMinimumApplicationAddress = %#x\n"),
-								systemInfo.lpMinimumApplicationAddress);
+		systemInfo.lpMinimumApplicationAddress);
 	_tprintf(_T("lpMaximumApplicationAddress = %#x\n"),
-								systemInfo.lpMaximumApplicationAddress);
+		systemInfo.lpMaximumApplicationAddress);
 
 	_tprintf(_T("ullAvailPhys = %I64x\n"), memStatus.ullAvailPhys);
 	_tprintf(_T("ullAvailVirtual = %I64x\n"), memStatus.ullAvailVirtual);
@@ -78,7 +78,7 @@ VOID CreateRegionList(PRLIST listFree, PRLIST listBusy)
 	HANDLE h = GetCurrentProcess();
 
 	// проходим по всем адресам приложения
-	for(size_t i = startAddr; i < endAddr; )
+	for (size_t i = startAddr; i < endAddr; )
 	{
 		MEMORY_BASIC_INFORMATION mInfo;
 
@@ -92,15 +92,17 @@ VOID CreateRegionList(PRLIST listFree, PRLIST listBusy)
 		curReg.size = mInfo.RegionSize;
 
 		//в зависимости от состояния записываем в 1 из 2 списков
-		if(mInfo.State == MEM_FREE)
+		if (mInfo.State == MEM_FREE)
 		{
 			listFree->push_back(curReg);
-		} else {
+		}
+		else {
 			listBusy->push_back(curReg);
 		}
 
 		i += ALIGN(curReg.size);
 	}
+
 }
 
 // вывод списков свободных и занятых блоков
@@ -116,8 +118,83 @@ VOID PrintRegionsList(PRLIST listFree, PRLIST listBusy) {
 		_tprintf(_T("%p\t%x\n"),
 			i->address, i->size);
 	}
-
 }
+
+
+bool compare_region_by_size(const REGION& lhs, const REGION& rhs) {
+	return lhs.size < rhs.size;
+}
+
+LPVOID AllocateMemory(size_t memorySize)
+{
+	if (memorySize == 0)
+		return nullptr;
+
+	HANDLE h = GetCurrentProcess();
+
+	RLIST listFree[1];
+	RLIST listBusy[1];
+
+	CreateRegionList(listFree, listBusy);
+
+	if (listFree->size() == 0)
+		return nullptr;
+
+	listFree->sort(compare_region_by_size);
+
+	RLIST::iterator i;
+
+	for (i = listFree->begin(); i != listFree->end(); i++) {
+		if (i->size > memorySize)
+			break;
+	}
+
+	if (i == listFree->end())
+		return nullptr;
+
+	LPVOID res = VirtualAllocEx(h, i->address, memorySize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+	/* для проверки, чтоб посмотреть значения переменных
+	RLIST listFree1[1];
+	RLIST listBusy1[1];
+	CreateRegionList(listFree1, listBusy1);
+	*/
+
+	return res;
+}
+
+BOOL FreeMemory(LPVOID address)
+{
+	if (address == nullptr)
+		return FALSE;
+
+	/* для проверки, чтоб посмотреть значения переменных
+	MEMORY_BASIC_INFORMATION mInfo;
+	RLIST listFree2[1];
+	RLIST listBusy2[1];
+	CreateRegionList(listFree2, listBusy2);
+	 */
+
+	HANDLE h = GetCurrentProcess();
+
+	/* для проверки, чтоб посмотреть значения переменных
+	VirtualQueryEx(h, (LPVOID)address, &mInfo, sizeof(mInfo));	 
+	BOOL b = mInfo.State == MEM_FREE;
+	*/
+
+	BOOL res = VirtualFreeEx(h, address, 0, MEM_RELEASE);
+
+	/* для проверки, чтоб посмотреть значения переменных
+	VirtualQueryEx(h, (LPVOID)address, &mInfo, sizeof(mInfo));
+	b = mInfo.State == MEM_FREE;
+	RLIST listFree3[1];
+	RLIST listBusy3[1];
+	CreateRegionList(listFree3, listBusy3);
+	*/
+
+	return res;
+}
+
 
 // --------------------Task3--------------------
 // страница
@@ -134,11 +211,11 @@ typedef list<PAGE> PAGES, *PPAGES;
 PVOID AddPage(PVOID *phAddresses, size_t count, PPAGES list, PPAGE page)
 {
 	PAGES::iterator it = list->begin();
-	while(it != list->end())
+	while (it != list->end())
 	{
 		// если страница была загружена в память,
 		// то удаляем ее, чтоб добавить  потом в конец
-		if(it->logAddress == page->logAddress)
+		if (it->logAddress == page->logAddress)
 		{
 			page->phAddress = it->phAddress;
 			list->erase(it);
@@ -148,15 +225,16 @@ PVOID AddPage(PVOID *phAddresses, size_t count, PPAGES list, PPAGE page)
 	}
 
 	// вышли из цикла, потому как дошли до конца
-	if(it == list->end())
+	if (it == list->end())
 	{
-		if(list->size() < count) // есть свободные позиции
+		if (list->size() < count) // есть свободные позиции
 		{
 			page->phAddress = phAddresses[list->size()];
-		} else {
+		}
+		else {
 			page->phAddress = list->begin()->phAddress;
 			list->pop_front();
-		}		
+		}
 	}
 	list->push_back(*page);
 	return page->phAddress;
@@ -166,43 +244,63 @@ PVOID AddPage(PVOID *phAddresses, size_t count, PPAGES list, PPAGE page)
 // --------------------Task4--------------------
 // четырехассоциативный кэш
 // функция изменения флагов
+
+/*
+ * b0, b1, b2 - биты в массиве flags
+ *
+ * 1 - left, 0 - right
+ *
+ *           b0
+ *         /    \
+ *		b1		  b2
+ *     /  \      /  \
+ *    0    1    2    3     <---- indexes
+ *
+ */
+
+ // изменение битов по индксу
 VOID ChangeFlags(BYTE &flags, BYTE bitIndex)
 {
-	if(bitIndex < 2)
+	if (bitIndex < 2)
 	{
-		flags |= 1;
-		if(bitIndex == 0)
+		flags |= 1; // |0|0|0|1|
+		if (bitIndex == 0)
 		{
-			flags |= 2;
-		} else {
-			flags &= ~2;
+			flags |= 2; // |0|0|1|1| left, left
 		}
-	} else {
-		flags &= ~1;
-		if(bitIndex == 2)
+		else {
+			flags &= ~2; // |1|1|0|1| left, right
+		}
+	}
+	else {
+		flags &= ~1;  // |1|1|1|0|
+		if (bitIndex == 2)
 		{
-			flags |= 4;
-		} else {
-			flags &= ~4;
+			flags |= 4; // |1|1|1|0| right, left
+		}
+		else {
+			flags &= ~4; // |1|0|1|0| right, right
 		}
 	}
 }
 
+// нахождение индекса по битам
 BYTE FindIndex(BYTE flags)
 {
 	BYTE index;
-	if(flags & 1) // работаем со второй половиной
+	if (flags & 1)
 	{
 		index = 2;
-		if(flags & 4)
+		if (flags & 4)
 		{
 			index++;
-		} else {
-			index = 0;
-			if(flags & 2)
-			{
-				index++;
-			}
+		}
+	}
+	else {
+		index = 0;
+		if (flags & 2)
+		{
+			index++;
 		}
 	}
 	return index;
@@ -212,22 +310,26 @@ BYTE FindIndex(BYTE flags)
 int main()
 {
 	PrintSystemInfo();
-	RLIST rfree[500];
-	RLIST rbusy[500];
+
+	LPVOID address = AllocateMemory(99999);
+
+	FreeMemory(address);
+
+	RLIST rfree[1];
+	RLIST rbusy[1];
 
 	CreateRegionList(rfree, rbusy);
+
 	PrintRegionsList(rfree, rbusy);
 
-	BYTE flags[1];
-	size_t index = 1;
-	
-	ChangeFlags(*flags, index);
+	BYTE flags;
+	BYTE index = 1;
 
-	BYTE res = FindIndex(*flags);
+	ChangeFlags(flags, index);
+
+	BYTE res = FindIndex(flags);
 
 	_tprintf(_T("\n%d\n"), res);
-
-
 
 
 	system("pause");
